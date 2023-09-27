a02f77641e9a178980d368e782c486568b960962
Leopoldo Medri <medribros@gmail.com>
2023-09-26 23:19:47 -0300

    List add

--- main.py
+++ main.py
@@ -22,18 +22,28 @@ barrios = data.barrio.to_list()
 barrios_adivinados = []
 
 while len(barrios_adivinados) < len(barrios):
-    answer_barrio = screen.textinput(title=f"Adivina el barrio {len(barrios_adivinados)}/{len(barrios)}",
-                                     prompt="Que barrio se te ocurre rey/reina?").title()
+    answer_barrio = screen.textinput(title=f"Adivina el barrio {len(barrios_adivinados)}/{len(barrios)}",prompt="Que barrio se te ocurre rey/reina?").title()
+
+    """Esta manera esta hecho con list comprehension"""
     if answer_barrio == "Exit":
-        barrios_sin_adivinar = []
-        """Si quedaron barrios sin adivinar,se appendea a una nueva lista
-        """
-        for barrio in barrios:
-            if barrio not in barrios_adivinados:
-                barrios_sin_adivinar.append(barrio)
+        barrios_sin_adivinar = [barrio for barrio in barrios if barrio not in barrios_adivinados]
         new_data = pd.DataFrame(barrios_sin_adivinar)
         new_data.to_csv("Barrios_por_conocer.csv")
         break
+
+    """Esta manera esta hecho con sin list comprehension"""
+
+    # if answer_barrio == "Exit":
+    #     barrios_sin_adivinar = []
+    #     """Si quedaron barrios sin adivinar,se appendea a una nueva lista
+    #     """
+    #     for barrio in barriose:
+    #         if barrio not in barrios_adivinados:
+    #             barrios_sin_adivinar.append(barrio)
+    #     new_data = pd.DataFrame(barrios_sin_adivinar)
+    #     new_data.to_csv("Barrios_por_conocer.csv")
+    #     break
+
     if answer_barrio in barrios:
         barrios_adivinados.append(answer_barrio)
         t = turtle.Turtle()
